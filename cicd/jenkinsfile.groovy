f12022b2ef9f09c73f7f6c65cd64b469a0031319
Ivan Vasquez <ivanvasquezp@outlook.com>
2021-12-01 14:02:58 -0700

    Update jenkinsfile.groovy

--- cicd/jenkinsfile.groovy
+++ cicd/jenkinsfile.groovy
@@ -1,34 +1,44 @@
 pipeline {
  agent any
  stages {
-        stage('Git Pull') {
-            steps {
-                git branch: 'master', url: 'https://github.com/IvanVasquez/LaravelCICD.git'
-            }
+    stage('Git Pull') {
+        steps {
+            git branch: 'master', url: 'https://github.com/IvanVasquez/LaravelCICD.git'
         }
-        stage("Build") {
-            steps{
-                sh 'php --version'
-                sh 'composer install'
-                sh 'composer --version'
-                withCredentials([file(credentialsId: 'env-cicd', variable: 'FILE')]) {
-                    sh 'cp $FILE .env'
-                    sh 'chmod 664 .env'
-                }
-                sh 'php artisan key:generate'
-                sh 'cp .env .env.testing'
-                sh 'php artisan migrate'
+    }
+    stage("Build") {
+        steps{
+            sh 'php --version'
+            sh 'composer install'
+            sh 'composer --version'
+            withCredentials([file(credentialsId: 'env-cicd', variable: 'FILE')]) {
+                sh 'cp $FILE .env'
+                sh 'chmod 664 .env'
             }
+            sh 'php artisan key:generate'
+            sh 'cp .env .env.testing'
+            sh 'php artisan migrate'
         }
-        stage("Unit test") {
-            steps {
-                sh 'php artisan test'
-            }
+    }
+    stage("Unit test") {
+        steps {
+            sh 'php artisan test'
         }
-        stage("Docker build") {
-            steps {
-                sh "docker build -t ivanvasquez0713/laravel-cicd ."
-            }
+    }
+    stage("Docker build") {
+        steps {
+            sh "docker build -t ivanvasquez0713/laravel-cicd ."
         }
+    }
+    stage("Docker push") {
+        environment {
+            DOCKER_USERNAME = credentials("docker-user")
+            DOCKER_PASSWORD = credentials("docker-password")
+        }
+        steps {
+            sh "docker login --username ${DOCKER_USERNAME} --password ${DOCKER_PASSWORD}"
+            sh "docker push danielgara/laravel8cd"
+        }
+    }
   }
 }

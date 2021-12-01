29e6c24f681e3e29c00abec69ccecd2f99ef345b
Ivan Vasquez <ivanvasquezp@outlook.com>
2021-12-01 13:10:03 -0700

    Update jenkinsfile.groovy

--- cicd/jenkinsfile.groovy
+++ cicd/jenkinsfile.groovy
@@ -2,6 +2,13 @@ pipeline {
  agent any
  stages {
         stage("Build") {
+            environment {
+                DB_HOST = credentials("laravel-host")
+                DB_PORT = credentials("laravel-port")
+                DB_DATABASE = credentials("laravel-database")
+                DB_USERNAME = credentials("laravel-user")
+                DB_PASSWORD = credentials("laravel-password")
+            }
             steps {
                 sh 'php --version'
                 sh 'composer install'

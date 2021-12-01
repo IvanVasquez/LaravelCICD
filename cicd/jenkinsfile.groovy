c4a4efad42bdb8b651eeebc337676f4cd5df9a10
Ivan Vasquez <ivanvasquezp@outlook.com>
2021-12-01 13:50:24 -0700

    Update jenkinsfile.groovy

--- cicd/jenkinsfile.groovy
+++ cicd/jenkinsfile.groovy
@@ -1,6 +1,11 @@
 pipeline {
  agent any
  stages {
+        stage('Git Pull') {
+            steps {
+                git branch: 'master', url: 'https://github.com/IvanVasquez/LaravelCICD.git'
+            }
+        }
         stage("Build") {
             steps{
                 sh 'php --version'
@@ -18,6 +23,11 @@ pipeline {
         stage("Unit test") {
             steps {
                 sh 'php artisan test'
+            }
+        }
+        stage("Docker build") {
+            steps {
+                sh "docker build -t ivanvasquez0713/laravel-cicd ."
             }
         }
   }

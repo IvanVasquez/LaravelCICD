3063ec04cf70b4b68c992c68db96e1c92750e65e
Ivan Vasquez <ivanvasquezp@outlook.com>
2021-12-02 08:56:43 -0700

    Update jenkinsfile.groovy

--- cicd/jenkinsfile.groovy
+++ cicd/jenkinsfile.groovy
@@ -1,10 +1,15 @@
 pipeline {
  agent any
  stages {
-    stage('Git Pull') {
+    stage("Git Pull") {
         steps {
             git branch: 'master', url: 'https://github.com/IvanVasquez/LaravelCICD.git'
         }
+    }
+    stage("Composer") {
+        sh 'php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"'
+        sh 'php composer-setup.php'
+        sh 'php -r "unlink(\'composer-setup.php\');"'
     }
     stage("Build") {
         steps{
